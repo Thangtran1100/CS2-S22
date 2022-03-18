174da9c8e55f6fd7e09c26be62d2adc7f74cfb63
Thangtran1100 <thangtrn117@gmail.com>
2022-03-18 10:48:38 -0700

    update3

---
+++ hw5/main.cpp
@@ -0,0 +1,20 @@
+#include "node.h"
+
+int main()
+{
+    LinkedList *node1 = new LinkedList;
+
+    node1->append(1);
+    node1->append(2);
+    node1->append(3);
+    node1->append(4);
+    node1->append(5);
+
+    node1->display();
+
+    node1->reverse();
+
+    node1->display();
+
+    return 0;
+}
