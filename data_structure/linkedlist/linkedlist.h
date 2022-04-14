7fbab15ee98f898642628b01a30e7e9af51a1b2f
Thangtran1100 <thangtrn117@gmail.com>
2022-04-14 15:16:36 -0700

    update

---
+++ data_structure/linkedlist/linkedlist.h
@@ -0,0 +1,16 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+class Linkedlist
+{
+private:
+    struct Node
+    {
+        int value;
+        
+        Node *next;
+    };
+
+
+};
+#endif
--- sample/main.cpp
+++ sample/main.cpp
@@ -7,42 +7,10 @@ struct Node
     Node *next;
 };
 
-void append(Node *head, int value)
-{
-    Node *node = new Node;
-
-    node->data = value;
-
-    node->next = nullptr;
-
-    // empty list
-    if (head == nullptr)
-    {
-        head = node;
-    }
-    else
-    {
-        Node *curr = head;
-
-        // loop till the last node of the list
-        while (curr->next != nullptr)
-        {
-            curr = curr->next;
-        }
-
-        curr->next = node; // add node as the new last node
-    }
-}
 
 int main()
 {
-    Node *head = nullptr;
-
-    Node *node1 = new Node;
-
-    Node *node2 = new Node;
-
-    Node *node3 = new Node;
+    
 
 
 }
