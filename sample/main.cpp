864739b1e20ad241d119427b53dec5f0d7c6bbe2
Thangtran1100 <thangtrn117@gmail.com>
2022-04-23 08:21:02 -0700

    change update

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,17 +1,28 @@
 #include<iostream>
+#include<vector>
 
-struct Node
-{
-    int data;
-
-    Node *next;
-};
-
+std::vector<int> digitize(unsigned long n) 
+{        
+  std::vector<int> dig;
+  
+    while (n != 0)
+    {
+        dig.push_back(n%10);
+        n = n / 10;
+    }
+  
+  
+  return dig;
+}
 
 int main()
 {
-    git add *.cpp *.h 
-git commit -m "update change"
-git push 
+    std::vector<int> d1;
+
+    d1 = digitize(35231);
+
+    std::cout << d1 << std::endl;
+
+    return 0;
 }
 
